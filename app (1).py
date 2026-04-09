2651aab5c7f58ef74157d02d73303c33638025ff
Ayushi Mangal <83300038+MANGAL2002@users.noreply.github.com>
2026-04-09 15:37:57 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -13,7 +13,7 @@ st.title("🧠 EEG Eye State Classification Dashboard")
 # ===============================
 # 📥 LOAD DATA FROM GITHUB
 # ===============================
-DATA_URL = "https://github.com/MANGAL2002/app/blob/main/EEG_Eye_State_Classification.csv"
+DATA_URL = "https://raw.githubusercontent.com/MANGAL2002/app/refs/heads/main/EEG_Eye_State_Classification.csv"
 
 @st.cache_data
 def load_data():
